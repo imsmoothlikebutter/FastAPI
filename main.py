1b61d0ff23542f08d96c358470e5d1485d4d06a2
Ismael Osman <muhammadismaelosman@gmail.com>
2024-03-15 11:57:04 +0800

    added GET route with extra params

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+from typing import Union
 from fastapi import FastAPI
 
 app = FastAPI()
@@ -5,3 +6,7 @@ app = FastAPI()
 @app.get("/home")
 def index():
     return {"message": "Hello, World"}
+
+@app.get("/home/{name}")
+def print_name(name: str, q: Union[str,None] = None):
+    return {"message": f"Hello, {name}, q: {q}"}
